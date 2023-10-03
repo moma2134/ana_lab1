ddd67891820242e2efbfad037544765dbda746c2
moma2134 <127178151+moma2134@users.noreply.github.com>
2023-10-02 23:10:55 -0600

    -- Added pages for Router Config and OSPF

--- code/app.py
+++ code/app.py
@@ -12,6 +12,29 @@ grafana_url = "http://localhost:3000/d/b513d541-02f4-4107-9249-95ff4054b63c/aris
 def index():
     return render_template('mainpage.html',grafana_url=grafana_url)
 
+# Webpage to add router config
+@app.route('/router_config', methods=['POST'])
+def router_config():
+    user_response = request.form.get('user_response')
+    if user_response == 'yes':
+        return render_template('add_router_config.html')
+    else:
+        return "No new router configuration added."
+    
+#Webpage to Handle OSPF Config Input
+@app.route('/configure_ospf', methods=['POST'])
+def configure_ospf():
+    ospf_id = request.form.get('ospf_id')
+    ospf_router_id = request.form.get('ospf_router_id')
+    ospf_networks = request.form.getlist('ospf_networks')
+    
+    return render_template(
+        'ospf_config.html',
+        ospf_id=ospf_id,
+        ospf_router_id=ospf_router_id,
+        ospf_networks=ospf_networks
+    )
+
 if __name__== "__main__":
     app.debug = True
     app.run(host='127.0.0.1',port=5000)
